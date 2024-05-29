8f25b86b04c8ecb47e5533f8b2b4f93e5749683e
Keagan <github@governorkeagan.com>
2024-05-29 03:44:40 +0100

    Added mastodon card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+// App.js
+import React from 'react';
+import MastodonToot from './components/MastodonToot';
 import './App.css';
 
+
 function App() {
+  const testUrl = 'https://en.osm.town/api/v1/statuses/112240497294603916';
+  const domain = 'https://en.osm.town';
+
   return (
-    <div>
-      <h1>Masto Img</h1>
+    <div className="app">
+      <h1 className="heading">Masto Img</h1>
+      <MastodonToot url={testUrl} domain={domain} />
     </div>
   );
 }
