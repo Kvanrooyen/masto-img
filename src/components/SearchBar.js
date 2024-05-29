a2ec80b044d4e0f34225762fdc07631f5543ac9d
Keagan <github@governorkeagan.com>
2024-05-29 15:56:12 +0100

    Added download button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,8 @@
-// SearchBar.js
 import React, { useState } from 'react';
 import './SearchBar.css';
 import { FaSearch } from 'react-icons/fa';
 
-const SearchBar = ({ onSearch }) => {
+const SearchBar = ({ onSearch, onDownload }) => {
   const [input, setInput] = useState('');
 
   const handleInputChange = (event) => {
@@ -16,18 +15,23 @@ const SearchBar = ({ onSearch }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="search-bar">
-      <input
-        type="text"
-        value={input}
-        onChange={handleInputChange}
-        placeholder="Enter Mastodon toot URL"
-        className="search-input"
-      />
-      <button type="submit" className="search-button">
-        <FaSearch />
+    <div>
+      <form onSubmit={handleSubmit} className="search-bar">
+        <input
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+          placeholder="Enter Mastodon toot URL"
+          className="search-input"
+        />
+        <button type="submit" className="search-button">
+          <FaSearch />
+        </button>
+      </form>
+      <button className="download-button" onClick={onDownload}>
+        Download Toot as Image
       </button>
-    </form>
+    </div>
   );
 };
 
